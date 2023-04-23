1850c3dd1878d843e76a70b49704b19d58a77352
CodingCat <zhunansjtu@gmail.com>
2023-04-22 21:07:04 -0700

    more progress

--- src/spot_advisor_processor.rs
+++ src/spot_advisor_processor.rs
@@ -3,8 +3,8 @@ use serde_json::{Value, Map};
 use reqwest::Error;
 use crate::constants::SPOT_ADVISOR_DATA;
 
-struct SpotInstanceInfo {
-    instance_type: String,
+#[derive(Debug)]
+pub struct SpotInstanceInfo {
     num_cores: i32,
     size_memory_in_gb: i32,
     preemption_rate: f32
@@ -28,24 +28,25 @@ fn parse_json_into_map(json_str: String) -> Map<String, Value> {
 }
 //println!("{:?}", hashmap);
 
-pub async fn process_spot_advisor_data() -> ()  {
+pub async fn process_spot_advisor_data() -> Result<HashMap<String, SpotInstanceInfo>, Error>  {
+    let mut instance_type_map: HashMap<String, SpotInstanceInfo> = HashMap::new();
     match get_json_content().await {
         Ok(file_content) => {
             let map = parse_json_into_map(file_content);
             for key in map.keys() {
                 match key.to_string().as_str() {
                     "instance_types" => {
-                        println!("{}", map.get("instance_types").unwrap().get("r5n.4xlarge").unwrap());
+                        let instance_map = map.get(key).unwrap().as_object().unwrap();
+                        for instance_type in instance_map.keys() {
+                            instance_type_map.insert(instance_type.clone(),
+                                 SpotInstanceInfo { num_cores: 0, size_memory_in_gb: 0, preemption_rate: 2.4 });
+                        }
                     },
                     _ => ()
                 }
             }
-            /* 
-            if let Some(value) = map.get("instance_types") {
-                println!("{}", value);
-            }  */ 
-            //println!("{:?}", map);
+            return Ok(instance_type_map);
         },
-        Err(e) => println!("Error: {}", e),
+        Err(e) => return Err(e),
     }
 }
